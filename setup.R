d70fee94a58d42020d15fac639b7ae9ca87a86dc
Francois Michonneau <francois.michonneau@gmail.com>
2015-03-19 15:22:26 +0200

    use single script across lesson to get lessons
    and load up knitr options

---
+++ setup.R
@@ -0,0 +1,14 @@
+## file structure
+
+if (! file.exists("data/")) dir.create("data")
+if (!file.exists("data/surveys.csv")) {
+    download.file("http://files.figshare.com/1919744/surveys.csv",
+                  "data/surveys.csv")
+}
+if (!file.exists("data/species.csv")) {
+    download.file("http://files.figshare.com/1919741/species.csv",
+                  "data/species.csv")
+}
+
+## knitr options
+knitr::opts_chunk$set(results='hide', fig.path='img/R-dplyr-')
